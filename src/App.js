0dd4a4b90792a3e0a13f3745f33d857b7b09cfaf
VS <varkisebi@gmail.com>
2021-07-09 22:12:12 +0530

    shift to new LandingPage component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
-import GlobalDashboard from './components/GlobalDashboard/GlobalDashboard'
+import LandingPage from './components/LandingPage/LandingPage'
 
-const App = () => {
-    return (
-        <Router>
-            <h1>Landing Page</h1>   
-            <Link to="/home">Home</Link>
-            <Link to="/global_dashboard">Global Dashboard</Link>
-
-            <Switch>
-                <Route path="/global_dashboard">
-                    <GlobalDashboard/>
-                </Route>
-            </Switch> 
-        </Router>
-    )
-}
+const App = () => <LandingPage />
 
 export default App
