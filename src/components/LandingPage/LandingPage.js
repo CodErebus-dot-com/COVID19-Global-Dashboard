c76f8a46fd382432ec62b3c927eb36739dc7dd70
VS <varkisebi@gmail.com>
2021-07-09 22:50:37 +0530

    final refactoring of LandingPage

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,20 +1,18 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
-import GlobalDashboard from '../GlobalDashboard/GlobalDashboard'
 
 const LandingPage = () => {
     return (
-        <Router>
+        <>
             <h1>Landing Page</h1>   
-            <Link to="/home">Home</Link>
-            <Link to="/global_dashboard">Global Dashboard</Link>
-
-            <Switch>
-                <Route path="/global_dashboard">
-                    <GlobalDashboard/>
-                </Route>
-            </Switch> 
-        </Router>
+            <div className="container">
+                <header>
+                    <span>UR COVID-19 App</span>
+                </header>
+            </div>
+            
+            
+             
+        </>
     )
 }
 
